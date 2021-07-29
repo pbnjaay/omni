48bab07c5e999b98515c18bea9523891c08a71ba
Papa bassirou ndiaye <pbnlgk@gmail.com>
2021-07-29 11:27:41 -0300

    adding parcel

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
+    if (this.classList.contains("collapsible--expanded"))
+      document.querySelector(".header").style.overflow = "visible";
+    else document.querySelector(".header").style.overflow = "hidden";
   })
 );
+
+console.log("working.....");
